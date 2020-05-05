991f9274112b3d64ffb41d52c92ee81f16161460
Simon Warta <simon@warta.it>
2020-05-05 12:25:27 +0200

    Pull out errors module

--- src/de/mod.rs
+++ src/de/mod.rs
@@ -1,136 +1,17 @@
 //! Deserialize JSON data to a Rust data structure
 
-use std::{error, fmt};
+mod enum_;
+mod errors;
+mod map;
+mod seq;
+
+pub use errors::{Error, Result};
 
 use serde::de::{self, Visitor};
 
 use self::enum_::{StructVariantAccess, UnitVariantAccess};
 use self::map::MapAccess;
 use self::seq::SeqAccess;
-
-mod enum_;
-mod map;
-mod seq;
-
-/// Deserialization result
-pub type Result<T> = core::result::Result<T, Error>;
-
-/// This type represents all possible errors that can occur when deserializing JSON data
-#[derive(Debug, PartialEq)]
-pub enum Error {
-    /// EOF while parsing a list.
-    EofWhileParsingList,
-
-    /// EOF while parsing an object.
-    EofWhileParsingObject,
-
-    /// EOF while parsing a string.
-    EofWhileParsingString,
-
-    /// EOF while parsing a JSON value.
-    EofWhileParsingValue,
-
-    /// Expected this character to be a `':'`.
-    ExpectedColon,
-
-    /// Expected this character to be either a `','` or a `']'`.
-    ExpectedListCommaOrEnd,
-
-    /// Expected this character to be either a `','` or a `'}'`.
-    ExpectedObjectCommaOrEnd,
-
-    /// Expected to parse either a `true`, `false`, or a `null`.
-    ExpectedSomeIdent,
-
-    /// Expected this character to start a JSON value.
-    ExpectedSomeValue,
-
-    /// Invalid number.
-    InvalidNumber,
-
-    /// Invalid type
-    InvalidType,
-
-    /// Invalid unicode code point.
-    InvalidUnicodeCodePoint,
-
-    /// Object key is not a string.
-    KeyMustBeAString,
-
-    /// JSON has non-whitespace trailing characters after the value.
-    TrailingCharacters,
-
-    /// JSON has a comma after the last value in an array or map.
-    TrailingComma,
-
-    /// Custom error message from serde
-    Custom(String),
-
-    #[doc(hidden)]
-    __Extensible,
-}
-
-impl error::Error for Error {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        None
-    }
-
-    fn description(&self) -> &str {
-        "(use display)"
-    }
-}
-
-impl de::Error for Error {
-    fn custom<T>(msg: T) -> Self
-    where
-        T: fmt::Display,
-    {
-        Error::Custom(msg.to_string())
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Error::EofWhileParsingList => "EOF while parsing a list.",
-                Error::EofWhileParsingObject => "EOF while parsing an object.",
-                Error::EofWhileParsingString => "EOF while parsing a string.",
-                Error::EofWhileParsingValue => "EOF while parsing a JSON value.",
-                Error::ExpectedColon => "Expected this character to be a `':'`.",
-                Error::ExpectedListCommaOrEnd => {
-                    "Expected this character to be either a `','` or\
-                     a \
-                     `']'`."
-                }
-                Error::ExpectedObjectCommaOrEnd => {
-                    "Expected this character to be either a `','` \
-                     or a \
-                     `'}'`."
-                }
-                Error::ExpectedSomeIdent => {
-                    "Expected to parse either a `true`, `false`, or a \
-                     `null`."
-                }
-                Error::ExpectedSomeValue => "Expected this character to start a JSON value.",
-                Error::InvalidNumber => "Invalid number.",
-                Error::InvalidType => "Invalid type",
-                Error::InvalidUnicodeCodePoint => "Invalid unicode code point.",
-                Error::KeyMustBeAString => "Object key is not a string.",
-                Error::TrailingCharacters => {
-                    "JSON has non-whitespace trailing characters after \
-                     the \
-                     value."
-                }
-                Error::TrailingComma => "JSON has a comma after the last value in an array or map.",
-                Error::Custom(msg) => &msg,
-                _ => "Invalid JSON",
-            }
-        )
-    }
-}
 
 fn unescape(source: &[u8]) -> Result<String> {
     // TODO: implement unescaping

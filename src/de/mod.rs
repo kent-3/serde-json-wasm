8bc2471da83457c0e02110100d0bf26f46d69364
Mauro Lacy <mauro@lacy.com.es>
2020-11-22 12:54:41 +0100

    Add unit structs deserialization support

--- src/de/mod.rs
+++ src/de/mod.rs
@@ -414,12 +414,22 @@ impl<'a, 'de> de::Deserializer<'de> for &'a mut Deserializer<'de> {
         unreachable!()
     }
 
-    /// Unsupported. Use a more specific deserialize_* method
-    fn deserialize_unit_struct<V>(self, _name: &'static str, _visitor: V) -> Result<V::Value>
+    /// Supported for convenience. Resolves to any requested unit (i.e. empty) struct.
+    fn deserialize_unit_struct<V>(self, _name: &'static str, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
     {
-        unreachable!()
+        let peek = self.parse_whitespace().ok_or(Error::EofWhileParsingValue)?;
+
+        if peek == b'{' {
+            self.eat_char();
+            let ret = visitor.visit_unit()?;
+            // Check syntax / consume closing curly brace
+            self.end_map()?;
+            Ok(ret)
+        } else {
+            Err(Error::InvalidType)
+        }
     }
 
     /// Unsupported. We can’t parse newtypes because we don’t know the underlying type.
@@ -800,6 +810,22 @@ mod tests {
             from_str::<Xy>(r#"[10, 20, 30]"#),
             Err(crate::de::Error::TrailingCharacters)
         );
+    }
+
+    #[test]
+    fn struct_empty() {
+        #[derive(Debug, Deserialize, PartialEq)]
+        struct Empty {};
+
+        assert_eq!(from_str(r#"{}"#), Ok(Empty {}));
+    }
+
+    #[test]
+    fn struct_nothing() {
+        #[derive(Debug, Deserialize, PartialEq, Default)]
+        struct Nothing;
+
+        assert_eq!(from_str(r#"{}"#), Ok(Nothing));
     }
 
     #[test]
